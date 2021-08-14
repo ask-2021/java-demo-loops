514795dccc755f21c3acb27b8f2e8b94f9794048
AsthaSingh <asthasingh@gmail.com>
2021-08-14 14:17:55 -0500

    Refactoring code

---
+++ src/main/java/org/astha/LoopsPrograms.java
@@ -0,0 +1,10 @@
+package org.astha;
+
+public class LoopsPrograms {
+
+    public static void main(String[] args) {
+        System.out.println("Loops");
+    }
+}
+
+
---
+++ src/main/java/org/astha/Test.java
@@ -0,0 +1,5 @@
+package org.astha;
+
+public class Test {
+
+}
